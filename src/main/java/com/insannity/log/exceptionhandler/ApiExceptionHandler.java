175dbadfbcee7ff3db326249acd8b305d79b0fcd
insannity <32269793+insanniity@users.noreply.github.com>
2021-05-20 13:18:39 -0300

    Tratamento de Exception

---
+++ src/main/java/com/insannity/log/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.insannity.log.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.insannity.log.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Problem.Campo> campos = new ArrayList<>();
+		
+		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			campos.add(new Problem.Campo(nome, mensagem));
+		}
+		
+		Problem problema = new Problem();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo("Um ou mais campos estão invalidos, preencha corretamente e tente novamente");
+		problema.setCampos(campos);
+		
+		// TODO Auto-generated method stub
+		return super.handleExceptionInternal(ex, problema , headers, status, request);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleDomain (NegocioException ex, WebRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		Problem problema = new Problem();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo(ex.getMessage());
+		
+		return handleExceptionInternal(ex, problema , new HttpHeaders() , status, request);
+	}
+	
+	
+}
