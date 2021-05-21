8df0fe9784f2092b285edff9d367318cb2dc4ba2
insannity <32269793+insanniity@users.noreply.github.com>
2021-05-21 04:25:26 -0300

    Update ApiExceptionHandler.java

--- src/main/java/com/insannity/log/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/insannity/log/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.insannity.log.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +14,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import com.insannity.log.exception.NegocioException;
 
 @ControllerAdvice
@@ -39,7 +40,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		
 		Problem problema = new Problem();
 		problema.setStatus(status.value());
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		problema.setTitulo("Um ou mais campos estão invalidos, preencha corretamente e tente novamente");
 		problema.setCampos(campos);
 		
@@ -53,7 +54,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		
 		Problem problema = new Problem();
 		problema.setStatus(status.value());
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		problema.setTitulo(ex.getMessage());
 		
 		return handleExceptionInternal(ex, problema , new HttpHeaders() , status, request);
